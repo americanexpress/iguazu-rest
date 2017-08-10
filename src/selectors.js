0ea09b639b61525a31173ea580d3137cabcdc160
Maia Black <Maia.Black@aexp.com>
2017-08-10 13:48:26 -0700

    fix(selectors): do not statically import getToState in case it changes after config initialization

--- src/selectors.js
+++ src/selectors.js
@@ -3,38 +3,36 @@ import { Map as iMap } from 'immutable';
 import config from './config';
 import { getIdHash, getQueryHash } from './helpers/hash';
 
-const { getToState } = config;
-
 export function resourceIsLoaded({ resource, id }) {
-  return state => !!getToState(state).getIn([resource, 'items', getIdHash(id)]);
+  return state => !!config.getToState(state).getIn([resource, 'items', getIdHash(id)]);
 }
 
 export function getResource({ resource, id }) {
   return (state) => {
-    const item = getToState(state).getIn([resource, 'items', getIdHash(id)]);
+    const item = config.getToState(state).getIn([resource, 'items', getIdHash(id)]);
     return item ? item.toJS() : item;
   };
 }
 
 export function resourceIsLoading({ resource, id }) {
-  return state => !!getToState(state).getIn([resource, 'loading', getIdHash(id)]);
+  return state => !!config.getToState(state).getIn([resource, 'loading', getIdHash(id)]);
 }
 
 export function getResourceLoadPromise({ resource, id }) {
-  return state => getToState(state).getIn([resource, 'loading', getIdHash(id)]);
+  return state => config.getToState(state).getIn([resource, 'loading', getIdHash(id)]);
 }
 
 export function collectionIsLoaded({ resource, id, opts }) {
   return (state) => {
     const idHash = getIdHash(id);
     const queryHash = getQueryHash(opts);
-    return !!getToState(state).getIn([resource, 'collections', idHash, queryHash]);
+    return !!config.getToState(state).getIn([resource, 'collections', idHash, queryHash]);
   };
 }
 
 export function getCollection({ resource, id, opts }) {
   return (state) => {
-    const resourceState = getToState(state).get(resource);
+    const resourceState = config.getToState(state).get(resource, iMap());
     const idHash = getIdHash(id);
     const queryHash = getQueryHash(opts);
     const { associatedIds, error } =
@@ -47,9 +45,9 @@ export function getCollection({ resource, id, opts }) {
 }
 
 export function collectionIsLoading({ resource, id, opts }) {
-  return state => !!getToState(state).getIn([resource, 'loading', getIdHash(id), getQueryHash(opts)]);
+  return state => !!config.getToState(state).getIn([resource, 'loading', getIdHash(id), getQueryHash(opts)]);
 }
 
 export function getCollectionLoadPromise({ resource, id, opts }) {
-  return state => getToState(state).getIn([resource, 'loading', getIdHash(id), getQueryHash(opts)]);
+  return state => config.getToState(state).getIn([resource, 'loading', getIdHash(id), getQueryHash(opts)]);
 }
